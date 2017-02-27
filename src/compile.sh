157c333ffc87d4c4e2044417bb95b583b775462d
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-02-27 22:50:52 +0100

    - update bash compile script

--- src/compile.sh
+++ src/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "compile core"
-g++ core.cpp -o core -lwiringPi libSonar.cpp
+g++ core.cpp -o core -lwiringPi ../lib/HC-SR04-Raspberry-Pi-C-/libSonar.cpp
 echo "compile commons"
 g++ reset_pins.cpp -o reset_pins -lwiringPi
 echo "compile photoresistor"
 g++ photoresistor.cpp -o photoresistor -lwiringPi
 echo "compile motor test"
-g++ test_motor.cpp -o test_motor -lwiringPi
+g++ test_motors.cpp -o test_motors -lwiringPi
