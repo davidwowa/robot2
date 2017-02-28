8f738e96a088d13040637232f7e104e5757b57f3
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-02-28 23:32:03 +0100

    - update compile script

--- src/compile.sh
+++ src/compile.sh
@@ -7,3 +7,14 @@ echo "compile photoresistor"
 g++ photoresistor.cpp -o photoresistor -lwiringPi
 echo "compile motor test"
 g++ test_motors.cpp -o test_motors -lwiringPi
+
+echo "compile motor test backward"
+g++ drive_backward.cpp -o drive_backward -lwiringPi
+echo "compile motor test forward"
+g++ drive_forward.cpp -o drive_forward -lwiringPi
+echo "compile motor test left"
+g++ drive_left.cpp -o drive_left -lwiringPi
+echo "compile motor test right"
+g++ drive_right.cpp -o drive_right -lwiringPi
+echo "compile motor test stop"
+g++ drive_stop.cpp -o drive_stop -lwiringPi
