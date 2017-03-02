bbd142b9b98e8acd6abf0b0e98477b474b58f6fc
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-03 00:31:14 +0100

    - deploy update for ilbs

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ echo "deploy robot2"
 echo "set rights"
 chmod +x src/compile.sh
 chmod +x ir-remote/setup.sh
+chmod +x lib/load_repos.sh
+echo "load libraries"
+lib/load_repos.sh
 echo "compile"
 src/compile.sh
 echo "setup ir"
