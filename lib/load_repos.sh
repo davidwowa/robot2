052d4207b1d2a899197ff44d4ff322d5d68b6c9d
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-03 03:02:27 +0100

    - update common - add opencv

--- lib/load_repos.sh
+++ lib/load_repos.sh
@@ -3,9 +3,11 @@ echo "load sonar library"
 git clone https://github.com/omaflak/HC-SR04-Raspberry-Pi-C-.git
 echo "load wiringPi library"
 git clone git://git.drogon.net/wiringPi
-echo "install wiringPi"
+#echo "install wiringPi"
 # only first time
 #cd wiringPi
 #git pull origin
 #cd wiringPi
 #./build
+#echo "load open cv library"
+#git clone https://github.com/opencv/opencv.git
--- src/common.h
+++ src/common.h
@@ -13,10 +13,10 @@ int maxWallDisntance_2 = 20;
 int maxWallDisntance_1 = 10;
 
 int speed_3 = 200;
-int speed_2 = 150;
-int speed_1 = 100;
+int speed_2 = 175;
+int speed_1 = 150;
 
-int interval = 650;
+int interval = 150;
 
 void set_working_pins_to_low(void);
 
