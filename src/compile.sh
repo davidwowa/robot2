73437f36d63aa26e9d140fa70b5f8cd02ef03e78
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-15 22:16:42 +0100

    - update compile script

--- src/compile.sh
+++ src/compile.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 echo "compile core"
-/home/pi/robot2/src/core/compile.sh
+cd /home/pi/robot2/src/core/
+./compile.sh
 
 echo "compile display test"
-/home/pi/robot2/src/core/test/display/compile.sh
+cd /home/pi/robot2/src/core/test/display/
+./compile.sh
 
 echo "compile sonar test"
-/home/pi/robot2/src/core/test/sonar/compile.sh
+cd /home/pi/robot2/src/core/test/sonar/
+./compile.sh
 
 echo "compile drive test"
-/home/pi/robot2/src/core/test/drive/compile.sh
+cd /home/pi/robot2/src/core/test/drive/
+./compile.sh
 
 echo "ready"
