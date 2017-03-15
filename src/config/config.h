d356f022153f966247bdb5c34420d9688577e249
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-15 20:38:02 +0100

    - new core

--- src/config/config.h
+++ src/config/config.h
@@ -8,13 +8,13 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-extern const int MAX_WALL_DISTANCE_3 = 30;
-extern const int MAX_WALL_DISTANCE_2 = 20;
-extern const int MAX_WALL_DISTANCE_1 = 10;
+extern const int MAX_WALL_DISTANCE_3 = 35;
+extern const int MAX_WALL_DISTANCE_2 = 25;
+extern const int MAX_WALL_DISTANCE_1 = 15;
 
 extern const int SPEED_3 = 200;
-extern const int SPEED_2 = 165;
-extern const int SPEED_1 = 125;
+extern const int SPEED_2 = 170;
+extern const int SPEED_1 = 150;
 
 extern const int FORWARD = 1;
 extern const int TURN_LEFT = 2;
