f71c251cbd42a9a5627a48b8dd232b5b36891fb7
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-15 17:23:45 +0100

    - update sonar test

--- src/test/sonar/sonar_test.cpp
+++ src/test/sonar/sonar_test.cpp
@@ -12,43 +12,29 @@
 #include "../../../lib/HC-SR04-Raspberry-Pi-C-/libSonar.cpp"
 
 #include "../../../lib/wiringPi/wiringPi/wiringPi.h"
-#include "../../../lib/wiringPi/wiringPi/softPwm.h"
 
 using namespace std;
 
-Sonar sonar_m_t;
-Sonar sonar_r_t;
-Sonar sonar_l_t;
-Sonar sonar_b_t;
-
-int DEBUG_t = 1;
-
 int main(void) {
-	if (DEBUG_t == 1) {
-		cout << "display show" << endl;
-	}
+	cout << "display show" << endl;
 	init_sonar();
 
 	init_display();
-	while (1) {
+	int i = 100;
 
-		int distance_m = sonar_m_t.distance(30000);
-		int distance_r = sonar_r_t.distance(30000);
-		int distance_l = sonar_l_t.distance(30000);
-		int distance_b = sonar_b_t.distance(30000);
+	while (i <= 100) {
 
-		if (DEBUG_t == 1) {
-			cout << "Distance on middle sensor is " << distance_m << " cm."
-					<< endl;
-			cout << "Distance on right  sensor is " << distance_r << " cm."
-					<< endl;
-			cout << "Distance on left   sensor is " << distance_l << " cm."
-					<< endl;
-			cout << "Distance on back   sensor is " << distance_b << " cm."
-					<< endl;
-			cout << "---" << endl;
-		}
+		int distance_m = get_distance_m();
+		int distance_r = get_distance_r();
+		int distance_l = get_distance_l();
+		int distance_b = get_distance_b();
 
+		cout << "Distance on middle sensor is " << distance_m << " cm." << endl;
+		cout << "Distance on right  sensor is " << distance_r << " cm." << endl;
+		cout << "Distance on left   sensor is " << distance_l << " cm." << endl;
+		cout << "Distance on back   sensor is " << distance_b << " cm." << endl;
+		cout << "---" << endl;
+		i++;
 		display_data(0, 0, distance_m, distance_r, distance_l, distance_b);
 	}
 	return -1;
