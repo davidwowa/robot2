8c1fe8f45359c80fcbdc3ac1d30b36810f6645ec
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-15 16:47:31 +0100

    - new test drive

--- src/test/drive/test_motors.cpp
+++ src/test/drive/test_motors.cpp
@@ -8,14 +8,11 @@
 #include "iostream"
 
 #include "../../drive/drive.h"
-#include "../../drive/gpio_drive.h"
 
 #include "../../../lib/wiringPi/wiringPi/wiringPi.h"
 #include "../../../lib/wiringPi/wiringPi/softPwm.h"
 
 using namespace std;
-
-int DEBUG = 1;
 
 int main(void) {
 	init_wiringPi();
@@ -23,60 +20,34 @@ int main(void) {
 	init_drive();
 
 	// acceleration forward
-	if (DEBUG == 1) {
-		cout << "acceleration forward" << endl;
-	}
-	for (int var = 0; var < PWM_MAX; ++var) {
-		if (DEBUG == 1) {
-			cout << "set speed to " << var << endl;
-		}
-		softPwmWrite(MOTOR_R_U, (PWM_MAX - var));
-		softPwmWrite(MOTOR_R_V, var);
-
-		softPwmWrite(MOTOR_L_U, var);
-		softPwmWrite(MOTOR_L_V, (PWM_MAX - var));
+	cout << "acceleration forward" << endl;
+	for (int var = 0; var < 200; ++var) {
+		cout << "set speed to " << var << endl;
+		drive_forward(var);
 		delay(100);
 	}
 	// acceleration backward
-	if (DEBUG == 1) {
-		cout << "acceleration backward" << endl;
-	}
-	for (int var = 0; var < PWM_MAX; ++var) {
-		if (DEBUG == 1) {
-			cout << "set speed to " << var << endl;
-		}
-		softPwmWrite(MOTOR_R_U, var);
-		softPwmWrite(MOTOR_R_V, (PWM_MAX - var));
-
-		softPwmWrite(MOTOR_L_U, (PWM_MAX - var));
-		softPwmWrite(MOTOR_L_V, var);
+	cout << "acceleration backward" << endl;
+	for (int var = 0; var < 200; ++var) {
+		cout << "set speed to " << var << endl;
+		drive_backward(var);
 		delay(100);
 	}
 
-	if (DEBUG == 1) {
-		cout << "drive forward" << endl;
-	}
+	cout << "drive forward" << endl;
 	drive_forward();
 	delay(3000);
-	if (DEBUG == 1) {
-		cout << "drive backward" << endl;
-	}
+	cout << "drive backward" << endl;
 	drive_backward();
 	delay(3000);
-	if (DEBUG == 1) {
-		cout << "drive right" << endl;
-	}
+	cout << "drive right" << endl;
 	turn_right();
 	delay(3000);
-	if (DEBUG == 1) {
-		cout << "drive left" << endl;
-	}
+	cout << "drive left" << endl;
 	turn_left();
 	delay(3000);
 
-	if (DEBUG == 1) {
-		cout << "stop" << endl;
-	}
+	cout << "stop" << endl;
 	stop_motors();
 	return -1;
 }
