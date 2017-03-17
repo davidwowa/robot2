ef8b181822590c3649c0af339ba65dd7400f0d47
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-17 02:12:13 +0100

    - update start and stop scripts

--- ir-remote/core.sh
+++ ir-remote/core.sh
@@ -2,5 +2,6 @@
 echo "kill core"
 sudo kill $(pidof core)
 echo "start core"
-sudo echo "1" > /home/pi/robot2/config/core
+sudo cat > /home/pi/robot2/config/core << EOF 1 EOF
+#sudo echo "1" > /home/pi/robot2/config/core
 sudo /home/pi/robot2/src/core/core
--- ir-remote/stop.sh
+++ ir-remote/stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "stop"
-sudo echo "0" > /home/pi/robot2/config/core
+sudo cat > /home/pi/robot2/config/core << EOF 0 EOF
+#sudo echo "0" > /home/pi/robot2/config/core
 sudo /home/pi/robot2/src/test_units/drive/stop
