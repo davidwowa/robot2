ddb4824f4213d8863e7b1c3042fc92648fb1f6c0
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-03-17 02:42:51 +0100

    - update project libs and core shell

--- ir-remote/core.sh
+++ ir-remote/core.sh
@@ -2,6 +2,10 @@
 echo "kill core"
 sudo kill $(pidof core)
 echo "start core"
-sudo cat > /home/pi/robot2/config/core << EOF 1 EOF
+if true ; then
+	sudo cat << EOF > /home/pi/robot2/config/core
+1
+EOF
+fi
 #sudo echo "1" > /home/pi/robot2/config/core
 sudo /home/pi/robot2/src/core/core
