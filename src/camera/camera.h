79bfc11e6b1fff4a81a0be0097f8dff4efcb7bf0
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-04-12 01:12:04 +0200

    - update opencv, pointer on face

---
+++ src/camera/camera.h
@@ -0,0 +1,13 @@
+/*
+ * camera.h
+ *
+ *  Created on: 12.04.2017
+ *      Author: Wladimir David Zakrevskyy
+ */
+
+#ifndef CAMERA_CAMERA_H_
+#define CAMERA_CAMERA_H_
+
+extern void take_pic();
+
+#endif /* CAMERA_CAMERA_H_ */
--- src/test_units/camera/compile.sh
+++ src/test_units/camera/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 g++ camera_test.cpp -o camera_test -I/usr/local/include -L/opt/vc/lib -lraspicam -lmmal -lmmal_core -lmmal_util ../../display/display.cpp -lwiringPi -lpthread -lArduiPi_OLED
 g++ camera_test_cv.cpp -o camera_test_cv -I/usr/local/include/ -L/opt/vc/lib -lraspicam -lraspicam_cv -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui -lopencv_imgcodecs ../../display/display.cpp -lArduiPi_OLED
+g++ opencv_face.cpp -o opencv_face -I/usr/local/include/ -L/opt/vc/lib -lraspicam -lraspicam_cv -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui -lopencv_imgcodecs ../../display/display.cpp -lArduiPi_OLED
 
 # after compiling to use camera with opencv 
 # copy 
